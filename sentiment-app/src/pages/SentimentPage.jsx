edd29ff1795a1def808fbb621f563567abf4ac89
Alphasf9 <134485025+Alphasf9@users.noreply.github.com>
2025-05-15 12:16:58 +0530

    commit frontend

--- sentiment-app/src/pages/SentimentPage.jsx
+++ sentiment-app/src/pages/SentimentPage.jsx
@@ -9,7 +9,7 @@ function SentimentPage() {
         if (!tweet.trim()) return;
         setLoading(true);
         try {
-            const response = await fetch('http://127.0.0.1:5000/predict', {
+            const response = await fetch('https://sentiment-analysis-naive.onrender.com/predict', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
